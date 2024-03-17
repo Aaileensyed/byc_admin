ec2ace91ac41a015c9458bfddf9af2ea99c5d2d1
Aaileensyed <aaileensyed@gmail.com>
2024-03-17 12:52:53 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,89 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontSize: {
+      "heading1-bold": [
+        "50px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading2-bold": [
+        "30px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading3-bold": [
+        "24px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading4-bold": [
+        "20px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "body-bold": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "body-semibold": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "600",
+        },
+      ],
+      "body-medium": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "500",
+        },
+      ],
+      "base-bold": [
+        "16px",
+        {
+          lineHeight: "100%",
+          fontWeight: "600",
+        },
+      ],
+      "base-medium": [
+        "16px",
+        {
+          lineHeight: "100%",
+          fontWeight: "500",
+        },
+      ],
+    },
+    extend: {
+      colors: {
+        "white-1":"#fdf5ef",
+        "grey-1": "#674b37",
+        "grey-2": "#e4d7cd",
+        "brown-1": "#231205",
+        "brown-2": "#faeadd",
+        "brown-3": "#fbfbf0",
+        "red-1": "#8b0000",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
